63368c208d8e0b89bb7e9ee8ce0c7709ad532059
Gladus96 <you@example.com>
2022-05-25 00:48:38 +0300

    My First

---
+++ myscript1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd d:QA_Test/
+mkdir q_1 q_2 q_3
+cd q_1/
+touch q11.txt q22.txt q33.txt q44.js q55.js
+mkdir one_one two_two one_two
+ls -la
+mv d:QA_Test/q_1/q11.txt d:QA_Test/q22.txt d:QA_Test/q_2
+
+
+
+
